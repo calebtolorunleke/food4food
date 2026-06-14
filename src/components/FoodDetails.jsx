d6a43260d1424d7ef53f04f2fe9c8308eb971b6c
Caleb Adebayo <calebtolorunleke@yahoo.com>
2026-06-13 23:19:45 -0700

    chore: update on Food4Food - food item selection

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+const API_KEY = "ffc9a2311d474fad96f62d0a3ca60238";
 
-const FoodDetails = ({ foodId }) => {
-  return <div>food details {foodId}</div>;
+const FoodDetails = ({ foodId, setFood }) => {
+  const URL = `http://api.spoonacular.com/recipes/${foodId}/information`;
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apikey=${API_KEY}`);
+      const data = res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+
+  return (
+    <div>
+      <div></div>
+      food details {foodId} {food.title} <img src={food.image} alt="" />
+    </div>
+  );
 };
 
 export default FoodDetails;
